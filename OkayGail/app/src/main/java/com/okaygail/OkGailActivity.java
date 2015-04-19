a991d4715fe87c4d1cedff6e4d5ca9bab28c22c0
Jason Safaiyeh <jsafaiyeh@gmail.com>
2015-04-19 01:22:45 -0700

    UI Completed... TODO: Implementation of NLP

--- OkayGail/app/src/main/java/com/okaygail/OkGailActivity.java
+++ OkayGail/app/src/main/java/com/okaygail/OkGailActivity.java
@@ -1,8 +1,10 @@
 package com.okaygail;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,9 +16,12 @@ import android.widget.Toolbar;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 
-public class OkGailActivity extends ActionBarActivity {
 
+public class OkGailActivity extends Activity {
+
+    private static final int SPEECH_REQUEST_CODE = 1;
     Toolbar mToolbar;
     TextView mTextView;
     Intent mIntent;
@@ -45,6 +50,10 @@ public class OkGailActivity extends ActionBarActivity {
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            /*    Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Gail is listening...");
+                startActivityForResult(i, SPEECH_REQUEST_CODE); */
                 Intent i = new Intent(mContext, PatientProfileActivity.class);
                 i.putExtra("Name", mIntent.getStringExtra("Name"));
                 startActivity(i);
@@ -52,4 +61,15 @@ public class OkGailActivity extends ActionBarActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            Intent i = new Intent(mContext, PatientProfileActivity.class);
+            i.putExtra("Height", results.get(0));
+            i.putExtra("Name", mIntent.getStringExtra("Name"));
+            startActivity(i);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
--- OkayGail/app/src/main/java/com/okaygail/PatientProfileActivity.java
+++ OkayGail/app/src/main/java/com/okaygail/PatientProfileActivity.java
@@ -1,24 +1,37 @@
 package com.okaygail;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 
-public class PatientProfileActivity extends ActionBarActivity {
+
+public class PatientProfileActivity extends Activity {
 
     Toolbar mToolbar;
     Intent mIntent;
+    TextView mPatientName, mDateOfBirth;
+    EditText mHeightEdit;
+    Button mConfirmButton;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
         this.getWindow().setStatusBarColor(getResources().getColor(R.color.color_primary_dark));
+        mContext = this;
 
         mIntent = getIntent();
 
@@ -26,8 +39,32 @@ public class PatientProfileActivity extends ActionBarActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setBackgroundColor(getResources().getColor(R.color.color_primary));
-        mToolbar.setTitle(mIntent.getStringExtra("Name"));
+        mToolbar.setTitle("Patient");
         mToolbar.setTitleTextColor(Color.WHITE);
 
+        //Patient Name
+        mPatientName = (TextView) findViewById(R.id.patient_profile_name);
+        mPatientName.setText(mIntent.getStringExtra("Name"));
+
+        //Date of Birth
+        mDateOfBirth = (TextView) findViewById(R.id.date_of_birth);
+        mDateOfBirth.setText("Date of Birth: 10/10/95");
+
+        //Height
+        String heightInfo = mIntent.getStringExtra("Height");
+        mHeightEdit = (EditText) findViewById(R.id.height_edit_text);
+//        mHeightEdit.setText(heightInfo.substring(7, 8) + "'" + heightInfo.substring(heightInfo.length() - 1));
+
+        mConfirmButton = (Button) findViewById(R.id.confirm_button);
+        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(mContext)
+                        .title("Send to Dr. Franklin")
+                        .positiveText("OK")
+                        .negativeText("Cancel")
+                        .show();
+            }
+        });
     }
 }
